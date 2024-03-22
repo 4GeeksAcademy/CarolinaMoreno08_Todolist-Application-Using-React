21c9da5464f525193f0aaee35bb92980a2bf519a
CarolinaMoreno08 <152432657+CarolinaMoreno08@users.noreply.github.com>
2024-03-22 01:29:04 +0000

    todos list app project

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,35 +1,54 @@
 import React, {useState} from "react";
 
 const Todolist = () => {
+
     const [inputValue, setInputValue] = useState("");
     const [todos, setTodos] = useState([]);
+
+            function saveChanges(e){
+                setInputValue(e.target.value);
+            }
+
+            function saveItems(e){
+                if (e.key == "Enter"){
+                    setTodos(todos.concat([inputValue])); 
+                    setInputValue("")
+                }
+            }
+
+            function deleteItems(idIndex){
+                let newItemsList = todos.filter((task, index) => idIndex != index)
+                setTodos(newItemsList)
+            }
 
     //Add into an array --> concat
     //Delete from array --> filter
     //Update --> map
 
-    function MouseOver(event) {
-        event.target.style.opacity=0;
-      }
-      function MouseOut(event){
-        event.target.style.opacity=3;
-      }
 
 
     return(
         <div className="container">
-            <h1 className="text-center">todos{inputValue}</h1>
-            <ul className="new-list">
+            <h1 className="text-center">todos</h1>
+            <div className="new-list">
                 <div className="container">
-                    {/* <input type="text" onChange={(e)=>setInputValue(e.target.value)} value ={inputValue}  placeholder="What needs to be done?" onKeyDown={(e)=> {if (e.key == "Enter"){setTodos(todos.concat([inputValue])); setInputValue("")}}}/> */}
                     <input type="text" 
-                    onChange={(e)=>setInputValue(e.target.value)} 
+                    onChange={saveChanges} 
                     value ={inputValue}  placeholder="What needs to be done?" 
-                    onKeyDown={(e)=> {if (e.key == "Enter"){setTodos(todos.concat([inputValue])); setInputValue("")}}}/>
+                    onKeyDown={saveItems}/>
                 </div>
-                {todos.map((item, index) => <div>{item}<button type="button" class="btn-close" aria-label="Close" onClick={()=> setTodos(todos.filter((t, currentIndex) => index != currentIndex))} onMouseOver={MouseOver} onMouseOut={MouseOut}></button></div> )}
-            </ul>
-            <div>{todos.length} items left</div>
+                {todos.map((item, idIndex) => {
+                    return (
+                        <div className="task">
+                        {item}
+                        <span className="delete-icon"
+                            onClick={() => deleteItems(idIndex)}>
+                        X </span>
+                        </div>
+                    )
+                })}
+            </div>
+            <div className="items-left">{todos.length} items left</div>
         </div>
     );
 };
@@ -37,3 +56,16 @@ const Todolist = () => {
 export default Todolist
 
 //line 19 without remove/delete the items from the list (before filtering) --> {todos.map((t) => <div>{t}<button type="button" class="btn-close" aria-label="Close"></button></div> )}
+
+// the code was like this before to create the functions (saveChanges & saveTasks):
+{/* <input type="text" 
+                    onChange={(e)=>setInputValue(e.target.value)} 
+                    value ={inputValue}  placeholder="What needs to be done?" 
+                    onKeyDown={(e)=> {if (e.key == "Enter"){setTodos(todos.concat([inputValue])); setInputValue("")}}}/> */}
+
+{/* 
+//the code was like this before to create the function: deleteItems.
+
+{todos.map((item, index) => <div>{item}<button type="button" class="btn-close" aria-label="Close" onClick={()=> setTodos(todos.filter((t, currentIndex) => index != currentIndex))} ></button></div> )} */}
+
+
